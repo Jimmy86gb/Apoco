7d61d5a52236d4203fa74567afb35a06a5c1eb7e
Jimmy86gb <Jimmy86gb@DESKTOP-6MC7PRJ>
2026-05-01 19:33:39 -0500

    Base MVC

---
+++ Apoco/src/main/java/co/edu/udistrital/model/Apoco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.udistrital.model;
+
+/**
+ *
+ * @author Jimmy86gb
+ */
+public class Apoco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
