e4d94f6ce72db27597f8fec228f67d9e3039c081
Jimmy86gb <Jimmy86gb@DESKTOP-6MC7PRJ>
2026-05-02 09:42:31 -0500

    Lista Simplemente Enlazada

---
+++ src/main/java/co/edu/udistrital/model/structures/SimpleList.java
@@ -0,0 +1,56 @@
+package co.edu.udistrital.model.structures;
+
+/**
+ *
+ * @author Jimmy86gb
+ * @param <T>
+ */
+public class SimpleList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+    
+    public SimpleList(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    
+    public void add(T data){
+        Node<T> newNode = new Node<>(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+        size++;
+    }
+    
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public void setHead(Node<T> head) {
+        this.head = head;
+    }
+
+    public Node<T> getTail() {
+        return tail;
+    }
+
+    public void setTail(Node<T> tail) {
+        this.tail = tail;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    
+}
